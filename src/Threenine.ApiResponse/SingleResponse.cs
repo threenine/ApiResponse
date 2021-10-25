c3b70ac67a88e2d041e5a1ba418ae7b0c327bcf2
Gary Woodfine <gary.woodfine@threenine.co.uk>
2021-10-25 14:04:27 +0100

    Refactored to include more Explicity ListResponse and SingleResponse object

---
+++ src/Threenine.ApiResponse/SingleResponse.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace Threenine.ApiResponse
+{
+    public class SingleResponse<TModel> : BaseResponse, ISingleResponse<TModel> where TModel : class 
+    {
+        public SingleResponse(TModel model, IList<string> validationErrors = null) : base(validationErrors)
+        {
+            Item = model;
+        }
+        public TModel Item { get; }
+    }
+}
--- tests/Threenine.ApiResponse.Tests/ResponseTests.cs
+++ tests/Threenine.ApiResponse.Tests/ResponseTests.cs
@@ -10,23 +10,10 @@ namespace Threenine.ApiResponse.Tests
         [Fact]
         public void Should_have_default_properties()
         {
-            var testClass = new Response<DummyResponseClass>(null);
+            var testClass = new SingleResponse<DummyResponseClass>(null);
 
             testClass.Item.ShouldBeAssignableTo<DummyResponseClass>();
-            testClass.Links.ShouldBeAssignableTo<IEnumerable<Link>>();
-        }
-
-        [Fact]
-        public void Should_return_links_list()
-        {
-            var testClass = new  Response<DummyResponseClass>(null)
-            {
-                Links = Builder<Link>.CreateListOfSize(4).Build()
-                
-            };
-
-            testClass.Links.ShouldBeAssignableTo<IList<Link>>();
-            testClass.Links.Count.ShouldBe(4);
+           
         }
     }
     
