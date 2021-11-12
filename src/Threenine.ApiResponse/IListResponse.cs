3237bcfc4015652913071547e3a6bd5abb7c7f8a
Gary Woodfine <gary.woodfine@threenine.co.uk>
2021-11-12 08:48:38 +0000

    Added marker interfaces

--- src/Threenine.ApiResponse/IListResponse.cs
+++ src/Threenine.ApiResponse/IListResponse.cs
@@ -2,7 +2,16 @@ using System.Collections.Generic;
 
 namespace Threenine.ApiResponse
 {
-    public interface IListResponse<TModel> where TModel : class
+    /// <summary>
+    /// Marker interface to define list response
+    /// </summary>
+    public interface IListResponse{}
+    
+    /// <summary>
+    /// Define a list response with a payload
+    /// </summary>
+    /// <typeparam name="TModel"></typeparam>
+    public interface IListResponse<TModel> : IListResponse where TModel : class
     {
         IList<TModel> Items { get; } 
     }
--- src/Threenine.ApiResponse/ISingleResponse.cs
+++ src/Threenine.ApiResponse/ISingleResponse.cs
@@ -1,6 +1,15 @@
 namespace Threenine.ApiResponse
 {
-    public interface ISingleResponse<TModel> where TModel : class
+    /// <summary>
+    /// Marker interface to define a Single Response
+    /// </summary>
+    public interface ISingleResponse{}
+    
+    /// <summary>
+    /// Define a single response with a payload
+    /// </summary>
+    /// <typeparam name="TModel"></typeparam>
+    public interface ISingleResponse<out TModel> : ISingleResponse where TModel : class
     {
         TModel Item { get; }
     }
