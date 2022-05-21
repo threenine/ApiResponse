d00c58ccf41a1d3411d93aa096220f9f74fd9112
Gary Woodfine <gary.woodfine@threenine.co.uk>
2022-05-21 13:48:02 +0100

    Added initial  Error Key names

---
+++ src/Threenine.ApiResponse/ErrorKeyNames.cs
@@ -0,0 +1,9 @@
+namespace Threenine.ApiResponse;
+
+public static class ErrorKeyNames
+{
+    public const string Conflict = "conflict";
+    public const string Validation = "validation";
+    public const string NotFound = "not-found";
+    
+}
