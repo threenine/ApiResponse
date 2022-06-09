d0a239bfaafef12f45bad8586dcf8bad7c0af156
Gary Woodfine <gary.woodfine@threenine.co.uk>
2022-06-09 08:29:05 +0100

    Added Mapping ErrorKey

--- src/ApiResponse/ErrorKeyNames.cs
+++ src/ApiResponse/ErrorKeyNames.cs
@@ -5,4 +5,5 @@ public static class ErrorKeyNames
     public const string Conflict = "conflict";
     public const string Validation = "validation";
     public const string NotFound = "notfound";
+    public const string Mapping = "mapping";
 }
